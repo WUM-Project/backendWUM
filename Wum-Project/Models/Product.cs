7b9885f8773cfa9b87bdbadd2d20fb684aa4def9
vadimyff <solpo789@gmail.com>
2023-06-28 16:56:02 +0300

    Add Product to branch

---
+++ Wum-Project/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wum_Project.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
